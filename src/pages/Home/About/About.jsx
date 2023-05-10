9bb9d01f9e69436c1f76a90f46803917f8f11bee
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-10 16:35:04 +0600

    about-us section added

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -4,14 +4,17 @@ import image2 from '../../../assets/images/about_us/parts.jpg'
 const About = () => {
     return (
         <div className="hero min-h-screen">
-            <div className="hero-content flex-col lg:flex-row">
-                <div className='lg:w-1/2'>
-                    <img src={image1} className="max-w-sm rounded-lg shadow-2xl" />
+            <div className="hero-content flex-col lg:flex-row gap-8">
+                <div className='lg:w-1/2 relative'>
+                    <img src={image1} className="w-3/4 h-auto md:h-80 rounded-lg shadow-2xl" />
+                    <img src={image2} className="absolute right-5 h-auto md:h-72 top-1/2 w-1/2 rounded-lg shadow-2xl border-8 border-white" />
                 </div>
-                <div className='lg:w-1/2'>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className='lg:w-1/2 space-y-5'>
+                    <h4 className='text-xl text-orange-600 font-bold'>About Us</h4>
+                    <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <button className="btn btn-warning rounded-md">Get More Info</button>
                 </div>
             </div>
         </div>

d4b3d1d943db13dc343e8d5fd8efbd0fe20d8eca
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-12 02:06:53 +0600

    booking page created, CREATE operation done, user can book service and send order to database

--- src/pages/BookService/BookService.jsx
+++ src/pages/BookService/BookService.jsx
@@ -1,37 +1,77 @@
 import { useLoaderData } from "react-router-dom";
-import img from '../../assets/images/checkout/checkout.png'
+import image from '../../assets/images/checkout/checkout.png'
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const BookService = () => {
     const service = useLoaderData();
-    console.log(service);
-    const { title, price, service_id } = service;
+    const { title, price, img, _id} = service;
+    const { user } = useContext(AuthContext)
+
+    const handleBookService = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const date = form.date.value;
+        const bookingInfo = { customerName:name, email, phone, date,img, service_id: _id, service: title, servicePrice: price  };
+        console.log(bookingInfo);
+
+        fetch('http://localhost:5000/bookings', {
+            method: "POST",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify(bookingInfo)
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        
+
+
+
+
+    }
+
+
     return (
         <div>
-            <div className="relative border p-24 w-10/12 mx-auto rounded-xl" style={{ backgroundImage: `url(${img})` }}>
+            <div className="relative mt-10 md:mt-5 border p-24 w-11/12 md:w-10/12 mx-auto rounded-xl" style={{ backgroundImage: `url(${image})` }}>
                 <div className="absolute bottom-0 flex items-center gap-5 left-0 right-0 rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full">
-                    <h2 className="text-3xl font-bold text-white pl-6">Book Your Service for : {title}</h2>
+                    <h2 className="text-3xl font-bold text-white pl-6">Book Your Service : {title}</h2>
                 </div>
             </div>
 
-            <div className="border my-12 p-16 w-10/12 mx-auto bg-slate-200 rounded-xl">
-                <div className="w-8/12 mx-auto space-y-3">
-                    <form>
-                        <div className=" flex justify-center items-center gap-5">
-                            <input type="text" placeholder="First Name" className="input input-bordered w-full max-w-xs  rounded-md" required />
+            <div className="border my-12 p-8 w-11/12 md:w-10/12 mx-auto bg-slate-200 rounded-xl">
+                <div className="md:w-8/12 mx-auto">
+                    <form onSubmit={handleBookService} className=" space-y-3">
+                        <div className="grid grid-cols-2 gap-5">
+                            <div>
+                                <label htmlFor="Name">Name:</label>
+                                <input type="text" name="name" defaultValue={user?.name} placeholder="Name" className="input input-bordered w-full max-w-xs rounded-md mb-3 md:mb-0" required />
+                            </div>
+                            <div>
+                                <label htmlFor="Email">Email:</label>
+                                <input type="email" name="email" defaultValue={user?.email} placeholder="Your Email" className="input input-bordered w-full max-w-xs rounded-md" />
+                            </div>
+                            <div>
+                                <label htmlFor="Phone">Phone:</label>
+                                <input type="number" name="phone" defaultValue={user?.phone} placeholder="Your Phone" className="input input-bordered w-full max-w-xs rounded-md mb-3 md:mb-0" required />
+                            </div>
 
-                            <input type="text" placeholder="Last Name" className="input input-bordered w-full max-w-xs  rounded-md" />
-                        </div>
-                        <div className="flex justify-center items-center gap-5">
-                            <input type="text" placeholder="Your Phone" className="input input-bordered w-full max-w-xs  rounded-md" required />
-
-                            <input type="text" placeholder="Your Email" className="input input-bordered w-full max-w-xs  rounded-md" />
-                        </div>
-                        <div className="flex justify-center items-center">
-                            <textarea className="textarea textarea-bordered rounded-md w-full h-48" placeholder="Bio"></textarea>
+                            <div>
+                                <label htmlFor="Date">Data & Time: </label>
+                                <input type="datetime-local" name="date" className="input input-bordered w-full max-w-xs rounded-md text-gray-400" />
+                            </div>
                         </div>
                         <div>
-                            <button className="btn btn-block btn-warning text-white bg-orange-600 rounded-md mt-5">Book Your Service</button>
+                            <input type="submit" value="Book Your Service" className="btn btn-block btn-warning text-white bg-orange-600 rounded-md mt-5" />
                         </div>
+
                     </form>
 
                 </div>
